3e6853b77c7c09a88d4d1f0940b0348e38f44368
tatematsu-k <ynu1264@gmail.com>
2025-11-05 20:21:15 +0900

    Add cache configuration support to Config (default: memory cache enabled)

--- lib/email_domain_checker/config.rb
+++ lib/email_domain_checker/config.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative "cache"
+
 module EmailDomainChecker
   class Config
     DEFAULT_OPTIONS = {
@@ -11,7 +13,7 @@ module EmailDomainChecker
     }.freeze
 
     class << self
-      attr_accessor :default_options, :test_mode
+      attr_accessor :default_options, :test_mode, :cache_enabled, :cache_type, :cache_ttl, :cache_adapter, :cache_adapter_instance, :redis_client
 
       def configure(options = {}, &block)
         if block_given?
@@ -28,6 +30,12 @@ module EmailDomainChecker
       def reset
         @default_options = DEFAULT_OPTIONS.dup
         @test_mode = false
+        @cache_enabled = true
+        @cache_type = :memory
+        @cache_ttl = 3600
+        @cache_adapter = nil
+        @cache_adapter_instance = nil
+        @redis_client = nil
       end
 
       def test_mode=(value)
@@ -37,17 +45,123 @@ module EmailDomainChecker
       def test_mode?
         @test_mode == true
       end
+
+      def cache_enabled?
+        @cache_enabled == true
+      end
+
+      def cache_adapter
+        return nil unless cache_enabled?
+
+        # If custom adapter instance is set, use it directly
+        return @cache_adapter_instance if @cache_adapter_instance
+
+        # Otherwise, create adapter from type
+        @cache_adapter ||= Cache.create_adapter(type: cache_type, redis_client: redis_client)
+      end
+
+      def cache_adapter_instance=(instance)
+        validate_cache_adapter_instance!(instance)
+        @cache_adapter_instance = instance
+        # Reset the auto-created adapter when custom instance is set
+        @cache_adapter = nil
+      end
+
+      def cache_type=(value)
+        @cache_type = value
+        reset_cache_adapter_if_needed
+      end
+
+      def clear_cache
+        cache_adapter&.clear
+      end
+
+      def clear_cache_for_domain(domain)
+        return unless cache_enabled?
+
+        adapter = cache_adapter
+        return unless adapter
+
+        # Clear both MX and A record cache entries
+        dns_cache_keys_for_domain(domain).each do |key|
+          adapter.delete(key)
+        end
+      end
+
+      def reset_cache_adapter_if_needed
+        # Reset cache adapter when changing cache type (unless custom instance is set)
+        @cache_adapter = nil unless @cache_adapter_instance
+        # Clear cache_adapter_instance if setting a type (Symbol or String), not a Class
+        @cache_adapter_instance = nil if @cache_type.is_a?(Symbol) || @cache_type.is_a?(String)
+      end
+
+      def reset_cache_adapter_on_enabled_change(new_value, old_value)
+        @cache_adapter = nil if new_value != old_value
+      end
+
+      def reset_cache_adapter_if_redis
+        @cache_adapter = nil if @cache_type == :redis
+      end
+
+      def validate_cache_adapter_instance!(instance)
+        unless instance.nil? || instance.is_a?(Cache::BaseAdapter)
+          raise ArgumentError, "cache_adapter_instance must be an instance of EmailDomainChecker::Cache::BaseAdapter or nil"
+        end
+      end
+
+      private
+
+      def dns_cache_keys_for_domain(domain)
+        ["mx:#{domain}", "a:#{domain}"]
+      end
     end
 
-    attr_accessor :test_mode
+    attr_accessor :test_mode, :cache_enabled, :cache_type, :cache_ttl, :cache_adapter_instance, :redis_client
 
     def initialize
       @test_mode = self.class.test_mode || false
+      @cache_enabled = self.class.cache_enabled.nil? ? true : self.class.cache_enabled
+      @cache_type = self.class.cache_type || :memory
+      @cache_ttl = self.class.cache_ttl || 3600
+      @cache_adapter_instance = self.class.cache_adapter_instance
+      @redis_client = self.class.redis_client
     end
 
     def test_mode=(value)
       @test_mode = value
       self.class.test_mode = value
+    end
+
+    def cache_enabled=(value)
+      old_value = self.class.cache_enabled
+      @cache_enabled = value
+      self.class.cache_enabled = value
+      # Reset cache adapter when enabling/disabling cache
+      self.class.reset_cache_adapter_on_enabled_change(value, old_value)
+    end
+
+    def cache_type=(value)
+      @cache_type = value
+      self.class.cache_type = value
+      self.class.reset_cache_adapter_if_needed
+    end
+
+    def cache_adapter_instance=(instance)
+      self.class.validate_cache_adapter_instance!(instance)
+      @cache_adapter_instance = instance
+      self.class.cache_adapter_instance = instance
+    end
+
+    def cache_ttl=(value)
+      @cache_ttl = value
+      self.class.cache_ttl = value
+    end
+
+    def redis_client=(value)
+      @redis_client = value
+      self.class.redis_client = value
+      # Reset cache adapter when changing redis client
+      self.class.reset_cache_adapter_if_redis
     end
 
     reset
